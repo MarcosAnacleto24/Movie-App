9dd96b86fd31e8a97b4ab78955538c310632afd8
Marcos Anacleto <marcosanacleto66@gmail.com>
2026-04-02 10:51:24 -0300

    feat: implementação inicial da tela de onboarding
    Adicionado layout base e estrutura para a apresentação do app.

---
+++ app/src/main/java/com/example/movieapp/presenter/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.movieapp.presenter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/presenter/onboarding/OnboardingFragment.kt
@@ -0,0 +1,33 @@
+package com.example.movieapp.presenter.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+    private var _binding: FragmentOnboardingBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
