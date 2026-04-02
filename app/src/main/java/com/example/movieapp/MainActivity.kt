694b3db817e1f1fad597a9f509aedc12a2509d50
Marcos Anacleto <marcosanacleto66@gmail.com>
2026-04-02 09:48:46 -0300

    chore: configuração inicial do projeto e sistema de design

---
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
