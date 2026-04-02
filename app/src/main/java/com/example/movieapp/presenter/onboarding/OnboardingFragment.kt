7cbaa7b12e0c237db9913df25d7554cd1c4054c7
Marcos Anacleto <marcosanacleto66@gmail.com>
2026-04-02 12:51:49 -0300

    fix: remoção de import não utilizado

--- app/src/main/java/com/example/movieapp/presenter/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/onboarding/OnboardingFragment.kt
@@ -1,11 +1,10 @@
 package com.example.movieapp.presenter.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.movieapp.R
+import androidx.fragment.app.Fragment
 import com.example.movieapp.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
