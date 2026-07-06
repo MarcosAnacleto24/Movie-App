79fd1828b00a099460ffc3fb8ed7b85be8e94f27
Marcos Anacleto <marcosanacleto66@gmail.com>
2026-07-06 15:14:04 -0300

    build: alinha versões do Kotlin, KSP e dependências para estabilidade do projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id ("org.jetbrains.kotlin.android") version "2.3.21" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id ("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
