09127d2eda38bafc300f79c706b59bfc839e187b
Marcos Anacleto <marcosanacleto66@gmail.com>
2026-07-23 10:15:19 -0300

    feat(edit-profile): estruturar layout da tela de edição de perfil
    - Criado o arquivo `fragment_edit_profile.xml` utilizando `ConstraintLayout` e `NestedScrollView`.
    - Adicionados os campos de formulário para dados do usuário e botão de atualização de perfil.

--- app/src/main/java/com/example/movieapp/presenter/main/bottombar/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/main/bottombar/profile/ProfileFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentProfileBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +29,50 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListeners()
+
     }
+
+    private fun initListeners() {
+
+        binding.btnEditProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_menu_profile_to_editProfileFragment)
+        }
+
+        binding.btnNotification.setOnClickListener {
+
+        }
+
+        binding.btnDownload.setOnClickListener {
+            activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)?.selectedItemId =
+                R.id.menu_download
+        }
+
+        binding.btnSecurity.setOnClickListener {
+
+        }
+
+        binding.btnLanguage.setOnClickListener {
+
+        }
+
+        binding.btnDarkMode.setOnClickListener {
+
+        }
+
+        binding.btnHelpCenter.setOnClickListener {
+
+        }
+
+        binding.btnPrivacyPolicy.setOnClickListener {
+
+        }
+
+        binding.btnLogout.setOnClickListener {
+
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
